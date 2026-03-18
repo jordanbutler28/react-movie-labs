935e8e17b204abb5732128cf04f1ef1f6e52bb56
Jordan Butler <20106583@mail.wit.ie>
2026-03-18 22:54:04 +0000

    Changed layout of site header

--- movies/src/components/siteHeader/index.jsx
+++ movies/src/components/siteHeader/index.jsx
@@ -6,33 +6,41 @@ import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import IconButton from "@mui/material/IconButton";
+import AppLogo from '@mui/icons-material/MovieFilter';
 import Button from "@mui/material/Button";
 import MenuIcon from "@mui/icons-material/Menu";
 import MenuItem from "@mui/material/MenuItem";
 import Menu from "@mui/material/Menu";
 import useMediaQuery from "@mui/material/useMediaQuery";
 
+//creates spacer div to push content below the header
 const Offset = styled('div')(({ theme }) => theme.mixins.toolbar);
 
 const SiteHeader = () => {
+  //stores element menu is anchored to for positioning, null if menu is closed
+  //for mobile menu
   const [anchorEl, setAnchorEl] = useState(null);
-  const open = Boolean(anchorEl);
+  const open = Boolean(anchorEl); //checks if menu is open by checking if anchorEl is not null
 
   const theme = useTheme();
   const isMobile = useMediaQuery(theme.breakpoints.down("md"));
   
   const navigate = useNavigate();
+  const goHome = () => {navigate("/");};
 
   const menuOptions = [
-    { label: "Home", path: "/" },
     { label: "Favorites", path: "/movies/favorites" },
     { label: "Watchlist", path: "/movies/watchlist" },
+  ];
+
+  const movieMenuOptions = [
     { label: "Upcoming", path: "/movies/upcoming" },
     { label: "Top Rated", path: "/movies/top-rated" },
     { label: "In Cinemas", path: "/movies/in-cinemas" },
     { label: "Trending", path: "/movies/trending" },
   ];
 
+  //for mobile menu
   const handleMenuSelect = (pageURL) => {
     setAnchorEl(null);
     navigate(pageURL);
@@ -42,12 +50,28 @@ const SiteHeader = () => {
     setAnchorEl(event.currentTarget);
   };
 
+  //for movies menu
+  const [moviesAnchhor, setMoviesAnchor] = useState(null);
+  const moviesOpen = Boolean(moviesAnchhor);
+
+  const handleMoviesMenu = (event) => {
+    setMoviesAnchor(event.currentTarget);
+  };
+
+  const handleMoviesClose = () => {
+    setMoviesAnchor(null);
+  };
+
   return (
     <>
       <AppBar position="fixed" color="secondary">
         <Toolbar>
-          <Typography variant="h4" sx={{ flexGrow: 1 }}>
-            TMDB Client
+          <Typography 
+            variant="h4" 
+            onClick={goHome} 
+            sx={{ flexGrow: 1, cursor: "pointer", display: "flex", alignItems: "center", gap: 1 }}>
+            Media Hub
+            <AppLogo sx={{fontSize: 40, padding: 2}} />
           </Typography>
             {isMobile ? (
               <>
@@ -75,6 +99,14 @@ const SiteHeader = () => {
                   open={open}
                   onClose={() => setAnchorEl(null)}
                 >
+                  {movieMenuOptions.map((opt) => (
+                  <MenuItem
+                    key={opt.label}
+                    onClick={() => handleMenuSelect(opt.path)}
+                  >
+                    {opt.label}
+                  </MenuItem>
+                ))}
                   {menuOptions.map((opt) => (
                     <MenuItem
                       key={opt.label}
@@ -87,6 +119,30 @@ const SiteHeader = () => {
               </>
             ) : (
               <>
+                <Button
+                  color="inherit"
+                  onClick={handleMoviesMenu}>
+                    Movies
+                </Button>
+                <Menu
+                  id="movies-menu"
+                  anchorEl={moviesAnchhor}
+                  open={moviesOpen}
+                  onClose={handleMoviesClose}
+                >
+                  {movieMenuOptions.map((opt) => (
+                    <MenuItem
+                    key={opt.label}
+                    onClick={() => {
+                      handleMoviesClose();
+                      navigate(opt.path);
+                    }}
+                  >
+                    {opt.label}
+                  </MenuItem>
+                  ))}
+                </Menu>
+                
                 {menuOptions.map((opt) => (
                   <Button
                     key={opt.label}
