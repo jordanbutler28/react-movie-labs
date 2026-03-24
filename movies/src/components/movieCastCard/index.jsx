4591b5e6b79234b984976b075abf84ec588e719d
Jordan Butler <20106583@mail.wit.ie>
2026-03-24 15:22:19 +0000

    finished change in look of credits page

--- movies/src/components/movieCastCard/index.jsx
+++ movies/src/components/movieCastCard/index.jsx
@@ -8,31 +8,32 @@ import Paper from '@mui/material/Paper';
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
 import Avatar from "@mui/material/Avatar";
+import Stack from '@mui/material/Stack';
+
 
 export default function MovieCastCard({ credits }) {
     
     return ( 
-        /*<Card sx={{ maxWidth: 345, textAlign: 'center' }}>
-            <CardHeader title={credits.name} />
-            <CardMedia
-                component="img"
-                height="300"
-                image={`https://image.tmdb.org/t/p/w500${credits.profile_path}`}
-                alt={credits.name}
-            />
-            <CardContent>
-                <Typography>
-                    {credits.character}
-                </Typography>
-            </CardContent>
-        </Card>*/
-    <Grid container spacing={2}>
-        <Paper variant="elevation" square= "false">
-            <Avatar src={`https://image.tmdb.org/t/p/w500${credits.profile_path}`} sx={{ width: 80, height: 80 }}></Avatar>
-            <Typography variant="h5">{credits.name}</Typography>
-            <Typography>{credits.character}</Typography>
-        </Paper>
-    </Grid>
+        <Grid container spacing={2}>
+            <Grid item xs={12}>
+                <Paper variant="elevation" square= "false" 
+                    sx={{
+                        width: 300, 
+                        height: 100,
+                        padding: 2,
+                        display: 'flex', 
+                        alignItems: 'center'
+                        }}>
+                    <Stack direction="row" spacing={2} alignItems="center">
+                        <Avatar src={`https://image.tmdb.org/t/p/w500${credits.profile_path}`} sx={{ width: 95, height: 95}}></Avatar>
+                        <Stack>
+                            <Typography variant="h5">{credits.name}</Typography>
+                            <Typography>{credits.character}</Typography>
+                        </Stack>
+                    </Stack>
+                </Paper>
+            </Grid>
+        </Grid>
 
     );
 }
